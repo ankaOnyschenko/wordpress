ac005b77231b807cd410cb5b3614faaf89970c80
ankaOnyschenko <annaonyschenko@gmail.com>
2014-10-25 12:47:23 +0300

    add homework

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'homework2');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'anka');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', '3407');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
